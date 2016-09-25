d354fb5ca9afd5a47e63e3efa84540ced3e81e0a
John Coppola <jdcoppola@gmail.com>
2016-09-25 16:35:55 -0400

    adds basic js functionality for navigating through different sections every section has been created and has at least some text

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+    // Hide all sections except welcome on page load
+    $('.js-section').hide();
+    $('.welcome').show();
+
+    // When a navbar link is clicked, first hide all sections and then show the clicked link's corresponding section
+    $('.js-nav-link').on('click', function(){
+        $('.js-section').hide();
+
+        if ($(this).hasClass('js-welcome')) {
+            $('.welcome').show();
+        }
+        else if ($(this).hasClass('js-about')) {
+            $('.about').show();
+        }
+        else if ($(this).hasClass('js-skills')) {
+            $('.skills').show();
+        }
+        else if ($(this).hasClass('js-experience')) {
+            $('.experience').show();
+        }
+        else if ($(this).hasClass('js-projects')) {
+            $('.projects').show();
+        }
+        else if ($(this).hasClass('js-contact')) {
+            $('.contact').show();
+        }
+    });
+
+});
