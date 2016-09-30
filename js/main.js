5a94eb801f1d039e9c96a7c62c8fd2d01c4e74d6
John Coppola <jdcoppola@gmail.com>
2016-09-30 19:36:37 -0400

    begins responsive layout for mobile

--- js/main.js
+++ js/main.js
@@ -1,38 +1,44 @@
 $(document).ready(function(){
 
-    // Hide all sections except welcome on page load
-    $('.js-section').hide();
-    $('.welcome').show();
+    if ($(window).width() >= 1050) {
 
-    // When a navbar link is clicked, slide to correct panel
-    $('.js-nav-link').on('click', function(){
-        $('.js-nav-link').removeClass('js-selected');
-        $(this).addClass('js-selected');
+        // Hide all sections except welcome on page load
+        $('.js-section').hide();
+        $('.welcome').show();
 
-        if ($(this).hasClass('js-welcome')) {
-            $('.js-section').not('.welcome').slideUp(350);
-        }
-        else if ($(this).hasClass('js-about')) {
-            $('.about').slideDown(350);
-            if ($('.experience:hidden')) {
-                $('.js-section').not('.welcome, .about').slideUp(350);
+        // When a navbar link is clicked, slide to correct panel
+        $('.js-nav-link').on('click', function(){
+            $('.js-nav-link').removeClass('js-selected');
+            $(this).addClass('js-selected');
+
+            if ($(this).hasClass('js-welcome')) {
+                $('.js-section').not('.welcome').slideUp(350);
             }
-        }
-        else if ($(this).hasClass('js-experience')) {
-            $('.about, .experience').slideDown(350);
-            if ($('.projects:hidden')) {
-                $('.js-section').not('.welcome, .about, .experience').slideUp(350);
+            else if ($(this).hasClass('js-about')) {
+                $('.about').slideDown(350);
+                if ($('.experience:hidden')) {
+                    $('.js-section').not('.welcome, .about').slideUp(350);
+                }
             }
-        }
-        else if ($(this).hasClass('js-projects')) {
-            $('.about, .experience, .projects').slideDown(350);
-            if ($('.projects:hidden')) {
-                $('.js-section').not('.welcome, .about, .experience, .projects').slideUp(350);
+            else if ($(this).hasClass('js-experience')) {
+                $('.about, .experience').slideDown(350);
+                if ($('.projects:hidden')) {
+                    $('.js-section').not('.welcome, .about, .experience').slideUp(350);
+                }
             }
-        }
-        else if ($(this).hasClass('js-contact')) {
-            $('.about, .experience, .projects, .contact').slideDown(350);
-        }
-    });
+            else if ($(this).hasClass('js-projects')) {
+                $('.about, .experience, .projects').slideDown(350);
+                if ($('.projects:hidden')) {
+                    $('.js-section').not('.welcome, .about, .experience, .projects').slideUp(350);
+                }
+            }
+            else if ($(this).hasClass('js-contact')) {
+                $('.about, .experience, .projects, .contact').slideDown(350);
+            }
+        });
+
+    }
+
+
 
 });
